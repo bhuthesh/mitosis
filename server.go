341911ae6d58845d196ae4cf88970af1b6881911
jimt <jimteeuwen@gmail.com>
2013-03-26 10:20:53 +0100

    Fixes compile error in last pull request. The return value of strconv.ParseUint could not be assigned to `port`.
    Change the return type of he port number to uint instead of
    uint16. No need to be that specific.

--- server.go
+++ server.go
@@ -19,9 +19,9 @@ const (
 var magick = []byte("MTSS") // Protocol header, identifying a valid mitosis session.
 
 // spawnServer opens the sever's listening socket.
-func spawnServer(state *State, done chan<- bool) (uint16, error) {
+func spawnServer(state *State, done chan<- bool) (uint, error) {
 	var listener net.Listener
-	var port uint16
+	var port uint
 	var err error
 
 	port = defaultPort
@@ -31,15 +31,19 @@ func spawnServer(state *State, done chan<- bool) (uint16, error) {
 	if err != nil {
 		return 0, errors.New("Failed to open a listen port")
 	}
+
 	laddr := listener.Addr().String()
 	_, portstr, err := net.SplitHostPort(laddr)
 	if err != nil {
 		return 0, fmt.Errorf("Failed to get port from listen port address string: %s", laddr)
 	}
-	port, err = strconv.ParseUint(portstr, 10, 16)
+
+	n, err := strconv.ParseUint(portstr, 10, 16)
 	if err != nil {
 		return 0, fmt.Errorf("Bad port number string: %s", portstr)
 	}
+
+	port = uint(n)
 
 	go func() {
 		defer listener.Close()
